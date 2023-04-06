4ff25aa4b8a0dc94b6a8e567db57c135df1d41e8
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-04-07 00:06:25 +0100

    add other methods

--- median/findMedian.js
+++ median/findMedian.js
@@ -1,15 +1,31 @@
+// Method 1
+
 const arr = [0, 1, 2, 4, 6, 5, 3];
 
 function findTheMedian(arr) {
   let array = arr.sort((a, b) => a - b);
-  console.log(array);
+  let n = array.length / 2;
   if (array.length % 2 === 0) {
-    let n = array.length / 2;
     let median = (array[n - 1] + array[n]) / 2;
     console.log(median);
-  } else {
-    let median = Math.floor(array.length / 2);
+  } else if (array.length % 2 !== 0) {
+    let median = Math.floor(n);
     console.log(median);
   }
 }
 findTheMedian(arr);
+
+// Method 2
+
+function getTheMedian(arr) {
+  let array = arr.sort((a, b) => a - b);
+  let n = array.length / 2;
+  if (array.length % 2 == 0) {
+    let m = (array[n - 1] + array[n]) / 2;
+    console.log(m);
+  } else {
+    let m = Math.floor(n);
+    console.log(m);
+  }
+}
+getTheMedian([0, 1, 2, 4, 6, 5, 3]);
