3a2c18f1408290111068a52a18383626e38d8468
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-04-10 23:52:20 +0100

    add the minimaxsum algorithm

--- Mini-Max-Sum/index.js
+++ Mini-Max-Sum/index.js
@@ -1,3 +1,13 @@
-module.exports = function miniMaxSum(arr) {
-  return "0 0"; 
+function miniMaxSum(arr) {
+  let minSum = 0;
+  let maxSum = 0;
+  let array = arr.sort((a, b) => a - b);
+  for (let i = 0; i < array.length - 1; i++) {
+    minSum += array[i];
+  }
+  for (let i = 1; i < array.length; i++) {
+    maxSum += array[i];
+  }
+  console.log(minSum + " " + maxSum);
 }
+miniMaxSum([1, 3, 5, 7, 9]);
