914c6dba1358919da3879fa41aa4ce00c88b7018
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-05-01 00:05:47 +0100

    add apple and orange

---
+++ apple/appleOrange.js
@@ -0,0 +1,17 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  let countApple = 0,
+    countOrange = 0;
+  apples.forEach((apple) => {
+    if (a + apple >= s && a + apple <= t) {
+      countApple++;
+    }
+    console.log(countApple);
+  });
+  oranges.forEach((orange) => {
+    if (b + orange >= s && b + orange <= t) {
+      countOrange++;
+    }
+    console.log(countOrange);
+  });
+}
+countApplesAndOranges(7, 11, 5, 15, 3, 2);
